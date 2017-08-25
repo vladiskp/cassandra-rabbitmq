72d975e1299e5a1f66321fc5d084c3b911e0b954
vladislav.korenevskij <vladiskp@protonmail.com>
2017-08-25 14:58:12 +0300

    modeify phone number class, add phone number repository

--- src/main/com/intexsoft/test_web_mvc/entity/PhoneNumber.java
+++ src/main/com/intexsoft/test_web_mvc/entity/PhoneNumber.java
@@ -3,19 +3,27 @@ package main.com.intexsoft.test_web_mvc.entity;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.data.cassandra.mapping.PrimaryKey;
+import org.springframework.data.cassandra.mapping.Table;
+import org.springframework.data.cassandra.mapping.Column;
 
+@Table(value = "Test_Phone_Number")
 @ToString
 public class PhoneNumber {
 
+    @PrimaryKey
     @Getter @Setter
     private String number;
 
+    @Column
     @Getter @Setter
     private long subscriberId;
 
+    @Column
     @Getter @Setter
     private double balance;
 
+    @Column
     @Getter @Setter
     private String phoneOperator;
 

5ff34fbcd1403d251715d785d8b67f4599df9824
vladislav.korenevskij <vladiskp@protonmail.com>
2017-08-28 17:10:14 +0300

    modify make call feature

--- src/main/com/intexsoft/test_web_mvc/entity/CallRecord.java
+++ src/main/com/intexsoft/test_web_mvc/entity/CallRecord.java
@@ -1,5 +1,6 @@
 package main.com.intexsoft.test_web_mvc.entity;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -9,6 +10,7 @@ import org.springframework.data.cassandra.mapping.Table;
 
 @Table(value = "Test_Call_Record")
 @ToString
+@Builder
 public class CallRecord {
 
     @PrimaryKey
@@ -37,11 +39,11 @@ public class CallRecord {
 
     @Column
     @Getter @Setter
-    private boolean rateType;
+    private boolean locationType;
 
     @Column
     @Getter @Setter
-    private boolean callType;
+    private boolean operatorType;
 
     @Column
     @Getter @Setter
@@ -53,15 +55,15 @@ public class CallRecord {
 
     public CallRecord(){}
 
-    public CallRecord(long id, long subscriberId, long duration, long startTime, long endTime, double price, boolean rateType, boolean callType, String outNumber, String inNumber) {
+    public CallRecord(long id, long subscriberId, long duration, long startTime, long endTime, double price, boolean locationType, boolean operatorType, String outNumber, String inNumber) {
         this.id = id;
         this.subscriberId = subscriberId;
         this.duration = duration;
         this.startTime = startTime;
         this.endTime = endTime;
         this.price = price;
-        this.rateType = rateType;
-        this.callType = callType;
+        this.locationType = locationType;
+        this.operatorType = operatorType;
         this.outNumber = outNumber;
         this.inNumber = inNumber;
     }

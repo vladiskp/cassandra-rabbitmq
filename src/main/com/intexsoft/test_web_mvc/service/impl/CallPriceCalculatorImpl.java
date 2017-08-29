ad1204107f9113400e57c4a7989a9f268b0d6fb9
vladislav.korenevskij <vladiskp@protonmail.com>
2017-08-29 14:08:53 +0300

    remove phone operator and phone operator info medels

--- src/main/com/intexsoft/test_web_mvc/service/impl/CallPriceCalculatorImpl.java
+++ src/main/com/intexsoft/test_web_mvc/service/impl/CallPriceCalculatorImpl.java
@@ -1,10 +1,6 @@
 package main.com.intexsoft.test_web_mvc.service.impl;
 
-import main.com.intexsoft.test_web_mvc.entity.CallRecord;
-import main.com.intexsoft.test_web_mvc.entity.PhoneOperator;
 import main.com.intexsoft.test_web_mvc.service.CallPriceCalculator;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
 
 @Service
